d5e3310585d2ee4d01f4efa7c8c513ba1c7797bd
Emmanuel Etuk <emmaetuk007@gmail.com>
2025-08-08 12:34:33 +0100

    some changes in the index

--- script/carousel.js
+++ script/carousel.js
@@ -7,8 +7,29 @@
         interval: 5000,
         arrows: false,
         breakpoints: {
-            
-        }
+          700: {
+            perPage: 1,
+          },  
+        },
+        type: 'loop',
+        pagination: true,
     } );
+    splide.mount();
+  } );
+  // Second Splide
+  document.addEventListener( 'DOMContentLoaded', function() {
+    var splide = new Splide( '#ToolsSplide', {
+        type: 'loop',
+        perPage: 1,
+        autoplay: true,
+        interval: 4000,
+        arrows: false,
+        breakpoints: {
+          700: {
+            perPage: 1,
+          },
+        },
+        pagination: true,
+    });
     splide.mount();
   } );
