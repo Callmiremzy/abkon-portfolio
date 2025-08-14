6e8226112b5d7e0fee1bd88eba5c355b54f7687c
Emmanuel Etuk <emmaetuk007@gmail.com>
2025-08-14 13:56:47 +0100

    Index and some style chamges and added new lines of code

---
+++ script/odometer.js
@@ -0,0 +1,34 @@
+// 
+var yearsCount = 6;
+var projectsCount = 45;
+var clientCount = 76;
+
+var year = document.querySelector("#yearCount");
+var project = document.querySelector("#projectsCount");
+var client = document.querySelector("#clientCount");
+
+// Create three separate Odometer 
+var yearOdometer = new Odometer({
+  el: year,
+  format: "(,ddd)",
+  theme: "default"
+});
+
+var projectOdometer = new Odometer({
+  el: project,
+  format: "(,ddd)",
+  theme: "default"
+});
+
+var clientOdometer = new Odometer({
+  el: client,
+  format: "(,ddd)",
+  theme: "default"
+});
+
+// Update values
+setInterval(function () {
+  yearOdometer.update(yearsCount);
+  projectOdometer.update(projectsCount);
+  clientOdometer.update(clientCount);
+}, 2000);
---
+++ script/typed.js
@@ -0,0 +1,7 @@
+var typed = new Typed("#typedText", {
+  strings: ["Hi, I'm Abasiama Bassey,", "a Brand Design Expert,", "and an Enterpreneur."],
+  typeSpeed: 60,       // typing speed in ms
+  backSpeed: 40,       // backspacing speed
+  loop: true           // keeps looping
+});
+  
